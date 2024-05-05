94f653417d0f529cbf46c028320be70e29987113
Diego Sabillon <diegosabillon1@gmail.com>
2024-05-05 02:02:54 -0600

    cambios a web chart

--- index.js
+++ index.js
@@ -77,28 +77,7 @@ darkEl.addEventListener("click", (e) => {
   }
 });
 
-//charts
 
-  const ctx = document.getElementById('myChart').getContext('2d');
-
-  new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: ['Comunes', 'Raros', 'Epicos'],
-      datasets: [{
-        label: 'Collecionables',
-        data: [65, 42, 20],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
 
 
 const usuarios = document.getElementById('usuarios').getContext('2d');
@@ -106,41 +85,40 @@ const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 const data = {
   labels: labels,
   datasets: [{
-    label: 'conexiones activos',
+    label: 'Pasajeros por vuelo',
     data: [65, 59, 80, 81, 56, 55, 40],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
-    ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-    ],
-    borderWidth: 1
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    borderWidth: 1,
+    barThickness: 30
+  },
+  {
+    label: 'Proyectados',
+    data: [70, 65, 85, 90, 60, 60, 45], // Reemplaza estos valores con tus datos proyectados
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    borderWidth: 1,
+    barThickness: 30
   }]
 };
 
 const config = {
-    type: 'bar',
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
       }
     },
-  };
+    elements: {
+      bar: {
+        borderWidth: 1,
+        barThickness: 1, // Ajusta este valor según tus necesidades
+      }
+    }
+  },
+};
 new Chart(usuarios,config);
 
 
